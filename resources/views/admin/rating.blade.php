641a78d2e8a33001839508ccf3aedbc3d931a5da
thuydung03z <lethithuydung1511@gmail.com>
2024-04-23 08:45:57 +0700

    final

--- resources/views/admin/rating.blade.php
+++ resources/views/admin/rating.blade.php
@@ -1,71 +1,54 @@
-@foreach ($rating as $item)
-    <div class="w-100 py-2">
-        <div class="d-flex justify-content-between">
-            <div class="d-flex">
-                <img 
-                    src="{{ asset('img/'. $item->user->image->image_url) }}" 
-                    alt="Avatar" 
-                    class="account-btn rounded-circle object-fit-cover shadow me-2"
-                    style="width:50px;height:50px;"
-                >
-                <div class="">
-                    <p class="fs-6 text-black fw-bold">{{ $item->user->user_name }}</p>
-                    <ul class="m-0 p-0 d-flex list-unstyled align-items-center">
-                    @for ($i = 1; $i <= 5; $i++)
-                        @if ($item->rating / $i >= 1)
-                            <li class="text-warning"><i class="fa-solid fa-star fs-xs"></i></li>
-                        @else
-                            <li class="text-warning"><i class="fa-regular fa-star fs-xs"></i></li>
-                        @endif
-                    @endfor
-                    </ul>
-                </div>
-            </div>
-            <p class="text-gray-600 fs-sm text-end">{{ $item->created_at }}</p>
-        </div>
-        <div class="w-100 ps-16 pe-4">
-            <div class="fs-6 text-black py-2">{{ $item->review }}</div>
-            <div class="d-flex">
-                @if (isset($item->image1))
-                <div class="w-25 me-2">
-                    <img 
-                        src="{{asset('img/'.$item->image1->image_url) }}" 
-                        alt="" 
-                        style="aspect-ratio:1 / 1;"
-                        class=" w-100 object-fit-cover"
-                    >
-                </div>
-                @endif
-                @if (isset($item->image2))
-                <div class="w-25 me-2">
-                    <img 
-                        src="{{asset('img/'.$item->image2->image_url) }}" 
-                        alt="" 
-                        style="aspect-ratio:1 / 1;"
-                        class=" w-100 object-fit-cover"
-                    >
-                </div>
-                @endif
-                @if (isset($item->image3))
-                <div class="w-25 me-2">
-                    <img 
-                        src="{{asset('img/'.$item->image3->image_url) }}" 
-                        alt="" 
-                        style="aspect-ratio:1 / 1;"
-                        class=" w-100 object-fit-cover"
-                    >
-                </div>
-                @endif
-            </div>
-        </div>
+@extends('layouts.admin')
+@section('title', 'Quản lý đánh giá')
+@section('content')
+
+<div class="admin-content">
+    <p class="fw-bold text-success fs-4">DANH SÁCH ĐÁNH GIÁ SẢN PHẨM</p>
+    <div class="d-flex justify-content-start position-relative mb-2 mt-3">
+        <input style="width:40%;" id="search" type="text" class="py-2 fs-sm px-10 rounded-3 border border-black" placeholder="Tìm kiếm đánh giá của khách hàng">
+        <i class="fa-solid fs-6 fa-magnifying-glass text-black position-absolute start-0 mt-3 mx-3"></i>
     </div>
-    @if (isset($item->reply))
-    <div class="w-75 p-4 my-2 rounded-3 shadow-md ms-auto bg-success-subtle">
-        <p class="text-success fs-6 fw-bold">
-            <span class="text-white p-1 me-1 bg-success rounded-3">LOGO</span>
-            Quản trị viên
-        </p>
-        <div class="fs-6 text-black mt-4">{!! $item->reply->reply !!}</div>
-    </div>
-    @endif
-@endforeach
+    <table class="" id="data-table">
+        <thead>
+            <tr class="head">
+                <th>Mã đánh giá</th>
+                <th>Tên khách hàng</th>
+                <th>Mã đơn hàng</th>
+                <th>Sản phẩm</th>
+                <th>Đánh giá</th>
+                <th>Nội dung</th>
+                <th>Đăng tải</th>
+                <th>Thao tác</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($data as $rating)
+            <tr class="text-center align-items-center border-bottom py-2 list-unstyled m-0 d-flex fs-xs text-black">
+                <td>{{ $rating->id_rating }}</td>
+                <td>{{ $rating->user->user_name }}</td>
+                <td>{{ $rating->orderdetail->id_order }}</td>
+                <td>{{ $rating->orderdetail->product->product_name }}</td>
+                <td>{{ $rating->rating }}/5</td>
+                <td>{{ $rating->review ?? 'NULL' }}</td>
+                <td>{{ $rating->created_at }}</td>
+                <td>
+                    <div class="btn d-inline-block">
+                        <i class="fa-solid fa-comment-dots fs-6 text-primary"></i>
+                        @include('admin.components.reply', ['data' => $rating])
+                    </div>&ensp;
+                    <div class="btn">
+                        <i class="fa-solid fa-circle-info fs-6 text-success"></i>
+                        @include('admin.components.detailRating', [
+                            'data' => $rating,
+                        ])
+                    </div>
+                    
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+
+    </table>
+</div>
+
+@endsection
